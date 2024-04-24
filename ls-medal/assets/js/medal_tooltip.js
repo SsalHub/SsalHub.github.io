8d236c5728c614e98f235d0031c38db602887967
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-24 14:02:08 +0900

    fix : medal js fixing

--- ls-medal/assets/js/medal_tooltip.js
+++ ls-medal/assets/js/medal_tooltip.js
@@ -1,14 +1,35 @@
-function openTooltip(id, name, name_kr, imageSrc, onerrorSrc, dataList, updated_at) {
-    var parentDiv = document.getElementById(id);
+function openTooltip(parentElem, medalName, medalName_kr, dataList) {
+    // Init Variables
     let tooltip = document.createElement('div');
     let tooltipHeader = document.createElement('div');
     let tooltipArticle = document.createElement('table');
     let tooltipFooter = document.createElement('div');
+
+    // set class names
     tooltip.className = 'tooltip';
     tooltipHeader.className = 'tooltip_header';
     tooltipArticle.className = 'tooltip_article';
     tooltipFooter.className = 'tooltip_footer';
-    tooltipHeader.innerHTML += '<div><img src="' + imageSrc + '" onerror="this.src=\'' + onerrorSrc + '\'"></div><label><b>&lt;' + name_kr + '&gt;</b></label>';
+
+
+    // Tooltip Header
+    div = document.createElement(div);
+    img = document.createElement(img);
+    img.setAttribute("src", "/assets/img/lostsaga/medal/" + medalName + ".png");
+    label = document.createElement(label);
+    label.innerHTML = "<b>&lt; + " + medalName_kr + "&gt;</b>";
+    div.append(img);
+    tooltipHeader.append(div);
+    tooltipHeader.append(label);
+
+
+
+
+    // tooltipHeader.innerHTML += '<div><img src="'
+    //     + imageSrc + '" onerror="this.src=\''
+    //     + onerrorSrc + '\'"></div><label><b>&lt;'
+    //     + name_kr
+    //     + '&gt;</b></label>';
 
     tooltipArticle.innerHTML += '<fieldset><legend align="center">능력치</legend><tbody><tr class="gray_row">'
         + getTableData('무기스킬', dataList[4])
@@ -19,10 +40,9 @@ function openTooltip(id, name, name_kr, imageSrc, onerrorSrc, dataList, updated_
         + getTableData('이동속도', dataList[2]) + '</tr>' + '<tr class="white_row">'
         + getTableData('망토스킬', dataList[7])
         + getTableData('드랍피해감소', dataList[3]) + '</tr></tbody></fieldset>';
-
     tooltipFooter.innerHTML += '<span>last update : ' + updated_at + '</span>';
 
-    parentDiv.addEventListener('mousemove', (e) => {
+    parentElem.addEventListener('mousemove', (e) => {
         tooltip.style.left = e.clientX + 'px';
         tooltip.style.top = e.clientY + 'px';
     });
@@ -30,12 +50,11 @@ function openTooltip(id, name, name_kr, imageSrc, onerrorSrc, dataList, updated_
     tooltip.append(tooltipHeader);
     tooltip.append(tooltipArticle);
     tooltip.append(tooltipFooter);
-    parentDiv.append(tooltip);
+    parentElem.append(tooltip);
 }
 
-function closeTooltip(id) {
-    var parentDiv = document.getElementById(id);
-    var tooltip = parentDiv.querySelector('.tooltip');
+function closeTooltip(parentElem) {
+    var tooltip = parentElem.querySelector('.tooltip');
     tooltip.remove();
 }
 
@@ -47,5 +66,5 @@ function getTableData(dataType, data) {
         data = '+' + data;
         return '<td class="data">' + dataType + '<font>' + data + '</font></td>';
     }
-    return '오류';
+    return 'Error!';
 }
