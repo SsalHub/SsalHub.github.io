e01138b6cfc6c814608f5a860d77439cb4056cbf
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-25 11:16:33 +0900

    feat : tooltip js added, but not working

--- ls-medal/assets/js/medal_tooltip.js
+++ ls-medal/assets/js/medal_tooltip.js
@@ -1,70 +1,118 @@
-function openTooltip(parentElem, medalName, medalName_kr, dataList) {
-    // Init Variables
-    let tooltip = document.createElement('div');
-    let tooltipHeader = document.createElement('div');
-    let tooltipArticle = document.createElement('table');
-    let tooltipFooter = document.createElement('div');
+function openTooltip(parentElement, medalName, medalName_kr, dataList) {
+  // Init Variables
+  let tooltip = document.createElement("div");
+  let tooltipHeader = document.createElement("div");
+  let tooltipArticle = document.createElement("table");
+  let tooltipFooter = document.createElement("div");
 
-    // set class names
-    tooltip.className = 'tooltip';
-    tooltipHeader.className = 'tooltip_header';
-    tooltipArticle.className = 'tooltip_article';
-    tooltipFooter.className = 'tooltip_footer';
+  // set class names
+  tooltip.className = "tooltip";
+  tooltipHeader.className = "tooltip_header";
+  tooltipArticle.className = "tooltip_article";
+  //   tooltipFooter.className = "tooltip_footer";
 
+  // Tooltip Header
+  div = document.createElement(div);
+  img = document.createElement(img);
+  img.setAttribute("src", "/assets/img/lostsaga/medal/" + medalName + ".png");
+  label = document.createElement(label);
+  label.innerHTML = "<b>&lt; + " + medalName_kr + "&gt;</b>";
+  div.append(img);
+  tooltipHeader.append(div);
+  tooltipHeader.append(label);
 
-    // Tooltip Header
-    div = document.createElement(div);
-    img = document.createElement(img);
-    img.setAttribute("src", "/assets/img/lostsaga/medal/" + medalName + ".png");
-    label = document.createElement(label);
-    label.innerHTML = "<b>&lt; + " + medalName_kr + "&gt;</b>";
-    div.append(img);
-    tooltipHeader.append(div);
-    tooltipHeader.append(label);
+  // Tooltip Body (Article)
+  var fieldset = document.createElement("fieldset");
+  var legend = document.createElement("legend");
+  legend.setAttribute("algn", "center");
+  legend.innerHTML = "능력치";
+  fieldset.append(legend);
+  var tbody = createElement("tbody");
+  // init tr array
+  var tr = [];
+  // i == 0
+  var i = 0;
+  tr.join(createElement("tr"));
+  tr[i].setAttribute("class", "gray_row");
+  tr[i].innerHTML =
+    getTableData("무기스킬", dataList[4]) +
+    getTableData("기본공격력", dataList[0]);
+  tbody.append(tr[i]);
+  // i == 1
+  i += 1;
+  tr.join(createElement("tr"));
+  tr[i].setAttribute("class", "whilte_row");
+  tr[i].innerHTML =
+    getTableData("갑옷스킬", dataList[5]) +
+    getTableData("기본방어력", dataList[1]);
+  tbody.append(tr[i]);
+  // i == 2
+  i += 1;
+  tr.join(createElement("tr"));
+  tr[i].setAttribute("class", "gray_row");
+  tr[i].innerHTML =
+    getTableData("투구스킬", dataList[6]) +
+    getTableData("이동속도", dataList[2]);
+  tbody.append(tr[i]);
+  // i == 3
+  i += 1;
+  tr.join(createElement("tr"));
+  tr[i].setAttribute("class", "whilte_row");
+  tr[i].innerHTML =
+    getTableData("망토스킬", dataList[7]) +
+    getTableData("드랍피해감소", dataList[3]);
+  tbody.append(tr[i]);
+  fieldset.append(tbody);
+  tooltipArticle.append(fieldset);
 
+  // tooltipFooter.innerHTML += "<span>last update : " + updated_at + "</span>";
 
+  // tooltipHeader.innerHTML += '<div><img src="'
+  //     + imageSrc + '" onerror="this.src=\''
+  //     + onerrorSrc + '\'"></div><label><b>&lt;'
+  //     + name_kr
+  //     + '&gt;</b></label>';
 
+  //   tooltipArticle.innerHTML +=
+  //     '<fieldset><legend align="center">능력치</legend><tbody><tr class="gray_row">' +
+  //     getTableData("무기스킬", dataList[4]) +
+  //     getTableData("기본공격력", dataList[0]) +
+  //     "</tr>" +
+  //     '<tr class="white_row">' +
+  //     getTableData("갑옷스킬", dataList[5]) +
+  //     getTableData("기본방어력", dataList[1]) +
+  //     "</tr>" +
+  //     '<tr class="gray_row">' +
+  //     getTableData("투구스킬", dataList[6]) +
+  //     getTableData("이동속도", dataList[2]) +
+  //     "</tr>" +
+  //     '<tr class="white_row">' +
+  //     getTableData("망토스킬", dataList[7]) +
+  //     getTableData("드랍피해감소", dataList[3]) +
+  //     "</tr></tbody></fieldset>";
 
-    // tooltipHeader.innerHTML += '<div><img src="'
-    //     + imageSrc + '" onerror="this.src=\''
-    //     + onerrorSrc + '\'"></div><label><b>&lt;'
-    //     + name_kr
-    //     + '&gt;</b></label>';
+  parentElement.addEventListener("mousemove", (e) => {
+    tooltip.style.left = e.clientX + "px";
+    tooltip.style.top = e.clientY + "px";
+  });
 
-    tooltipArticle.innerHTML += '<fieldset><legend align="center">능력치</legend><tbody><tr class="gray_row">'
-        + getTableData('무기스킬', dataList[4])
-        + getTableData('기본공격력', dataList[0]) + '</tr>' + '<tr class="white_row">'
-        + getTableData('갑옷스킬', dataList[5])
-        + getTableData('기본방어력', dataList[1]) + '</tr>' + '<tr class="gray_row">'
-        + getTableData('투구스킬', dataList[6])
-        + getTableData('이동속도', dataList[2]) + '</tr>' + '<tr class="white_row">'
-        + getTableData('망토스킬', dataList[7])
-        + getTableData('드랍피해감소', dataList[3]) + '</tr></tbody></fieldset>';
-    tooltipFooter.innerHTML += '<span>last update : ' + updated_at + '</span>';
-
-    parentElem.addEventListener('mousemove', (e) => {
-        tooltip.style.left = e.clientX + 'px';
-        tooltip.style.top = e.clientY + 'px';
-    });
-
-    tooltip.append(tooltipHeader);
-    tooltip.append(tooltipArticle);
-    tooltip.append(tooltipFooter);
-    parentElem.append(tooltip);
+  tooltip.append(tooltipHeader);
+  tooltip.append(tooltipArticle);
+  //   tooltip.append(tooltipFooter);
+  parentElement.append(tooltip);
 }
 
-function closeTooltip(parentElem) {
-    var tooltip = parentElem.querySelector('.tooltip');
-    tooltip.remove();
+function closeTooltip(parentElement) {
+  var tooltip = parentElement.querySelector(".tooltip");
+  tooltip.remove();
 }
 
 function getTableData(dataType, data) {
-    if (data == '' || data == '0') {
-        return '<td class="no_data">' + dataType + '</td>';
-    }
-    else if (data[0] != '-') {
-        data = '+' + data;
-        return '<td class="data">' + dataType + '<font>' + data + '</font></td>';
-    }
-    return 'Error!';
+  if (data == "" || data == "0") {
+    return '<td class="no_data">' + dataType + "</td>";
+  } else if (data[0] != "-") {
+    data = "+" + data;
+    return '<td class="data">' + dataType + "<font>" + data + "</font></td>";
+  }
+  return "Error!";
 }
