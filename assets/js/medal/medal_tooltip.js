f91479fbe64a2414d9ae25684ce0963110d34a28
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-27 14:46:28 +0900

    feat : tooltip complete

--- assets/js/medal/medal_tooltip.js
+++ assets/js/medal/medal_tooltip.js
@@ -10,24 +10,53 @@ function appendTooltip(parentElement, medalName, medalName_kr, dataList) {
     // Init Variables
     let tooltip = document.createElement("div");
     tooltip.className = "tooltip";
-    let tooltipHeader = document.createElement("div");
-    tooltipHeader.className = "tooltip_header";
-    let tooltipArticle = document.createElement("table");
-    tooltipArticle.className = "tooltip_article";
     // let tooltipFooter = document.createElement("div");
     //   tooltipFooter.className = "tooltip_footer";
 
     // Tooltip Header
-    div = document.createElement("div");
-    img = document.createElement("img");
-    img.setAttribute("src", "/assets/img/lostsaga/medal/" + medalName + ".png");
-    label = document.createElement("label");
-    label.innerHTML = "<b>&lt; + " + medalName_kr + "&gt;</b>";
-    div.append(img);
-    tooltipHeader.append(div);
-    tooltipHeader.append(label);
+    tooltipHeader = initTooltipHeader(medalName, medalName_kr)
 
     // Tooltip Body (Article)
+    tooltipArticle = initTooltipArticle(dataList);
+
+    tooltip.append(tooltipHeader);
+    tooltip.append(tooltipArticle);
+    //   tooltip.append(tooltipFooter);
+
+    parentElement.append(tooltip);
+    parentElement.addEventListener("mousemove", (e) => {
+        tooltip.style.left = (e.pageX + 25) + "px"; // x pos by page
+        tooltip.style.top = (e.pageY - 120) + "px"; // y pos by page
+    });
+}
+
+/**
+ * Init Medal Tooltip Header 
+ * @param {String} medalName medal name
+ * @param {String} medalName_kr medal name in korean
+ */
+function initTooltipHeader(medalName, medalName_kr) {
+    let tooltipHeader = document.createElement("span");
+    tooltipHeader.className = "tooltip_header";
+
+    var img = document.createElement("img");
+    img.setAttribute("src", "/assets/img/lostsaga/medal/" + medalName + ".png");
+    var span = document.createElement("span");
+    span.innerHTML = "&lt;" + medalName_kr + "&gt;";
+    tooltipHeader.append(img);
+    tooltipHeader.append(span);
+
+    return tooltipHeader;
+}
+
+/**
+ * Init Medal Tooltip Table 
+ * @param {Object} dataList medal data array object
+ */
+function initTooltipArticle(dataList) {
+    let tooltipArticle = document.createElement("table");
+    tooltipArticle.className = "tooltip_article";
+
     var fieldset = document.createElement("fieldset");
     var legend = document.createElement("legend");
     legend.setAttribute("align", "center");
@@ -66,35 +95,23 @@ function appendTooltip(parentElement, medalName, medalName_kr, dataList) {
     tbody.append(tr[3]);
     fieldset.append(tbody);
     tooltipArticle.append(fieldset);
-
-    tooltip.append(tooltipHeader);
-    tooltip.append(tooltipArticle);
-    //   tooltip.append(tooltipFooter);
-
-    parentElement.append(tooltip);
-    parentElement.addEventListener("mousemove", (e) => {
-        tooltip.style.left = (e.pageX + 50) + "px"; // x pos by page
-        tooltip.style.top = (e.pageY - 120) + "px"; // y pos by page
-    });
+    return tooltipArticle;
 }
 
 /**
- * Get innerHTML Data With Medal Data/DataType 
- * @param {String} dataType medal data type
- * @param {String} data medal data
+ * Get innerHTML String 
+ * @param {String} dataType type of medal data
+ * @param {String} data medal medal
  */
 function getTableData(dataType, data) {
     if (data == "" || data == "0") {
-        return '<td class="no_data">' + dataType + "</td>";
+        return '<td class="no_data"><font>' + dataType + "</font></td>";
     } else if (data[0] != "-") {
         data = "+" + data;
         return '<td class="data"><font class="data_type">' + dataType + '</font><font class="data_text">' + data + '</font></td>';
     }
     return "Error!";
 }
-
-
-
 
 
 
